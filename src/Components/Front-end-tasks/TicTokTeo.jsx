9f2753cf06bc0345266de3ad28e3a9b17fd68cae
Uppari Praveen <upparipraveen2003@gmail.com>
2026-05-30 09:09:59 +0530

    tic tack teo

--- src/Components/Front-end-tasks/TicTokTeo.jsx
+++ src/Components/Front-end-tasks/TicTokTeo.jsx
@@ -1,47 +1,63 @@
-import React,{useState} from 'react'
+import React from 'react';
+import { useState } from 'react'
 
-const TicTokTeo = () => {
-    const matrix = [
-    [1,2,3],
-    [1,2,3],
-    [1,2,3]
-  ]
-    const [status, setStatus] = useState([]);
-    const [switchPlayer, setSwitchPlayer] = useState(true);
-  
-  const handleRowCol = (row, col) => {
-    console.log(row, col, "i am row");
-    console.log(status, "i am status");
-  
+function TicTokTeo() {
+  const[turn,setTurn] = useState(true)
+  const[grid,setGrid] = useState(Array(9).fill(""))
+  const handleClick =(index)=>{
+    // console.log(index)
+    if(grid[index] == "X" || grid[index] == "O") return ""
+    const newGrid = [...grid]
     
-  };
-  const changeColor = (row, col) => {
-    const exists = status.find(
-      (item) => item.rowcol[0] === row && item.rowcol[1] === col
-    );
-    return exists ? "red" : "blue";
+    if(turn){
+      newGrid[index] = "X"
+      setTurn(false)
+    }else{
+      newGrid[index] = "O"
+      setTurn(true)
+    }
+    const decideWinner = handleWinder(newGrid)
+    console.log(decideWinner,"Wiiner")
+    if (decideWinner){
+      alert(`${turn ? "X" : "O"}"is the Wiiner`)
+setGrid(Array.from({ length: 9 }, () => ""));      
+return ""
+    }
+    setGrid(newGrid)
   }
-   console.log(status)
+  const handleWinder = (grid) => {
+  let win = [
+    [0,1,2],[3,4,5],[6,7,8],
+    [0,3,6],[1,4,7],[2,5,8],
+    [0,4,8],[2,4,6]
+  ];
+
+  for (let [a, b, c] of win) {
+    if (grid[a] && grid[a] === grid[b] && grid[a] === grid[c]) {
+      return true;
+    }
+  }
+
+  return false;
+};
+    console.log(grid)
+
   return (
-     <div>
-      {matrix.map((item,row)=>{
-        
-        return(
-          <div key = {row} style={{display:'flex',alignItems:'center',justifyContent:"center",flexDirection:'row',margin:"10px",gap:"10px"}}
-          
-          >
-            
-            {item.map((colVal,col)=>{
-            
-              return(
-                <div onClick={(e)=>handleRowCol(row,col)} key={col} style={{gap:"10px",height:"50px",width:"50px",background:changeColor(row,col),display:"flex",alignItems:"center",justifyContent:"center",color:"white",cursor:"pointer"}}>
-                  {colVal}
-                </div>
-              )
-            })}
-          </div>
+    <div>
+     <h1>Hey i am tic tac teo</h1>
+     <div style={{
+      display: 'grid',
+      gridTemplateColumns: 'repeat(3, 50px)', // 3 columns
+      gap: '10px'
+    }}>
+       {grid.map((item,index)=>{
+         return (
+         <div onClick = {()=>handleClick(index)} style={{display:'flex',alignItems:"center",justifyContent:"center",background:"red",height:"50px",width:"50px"}}>
+           {item}
+           </div>
         )
-      })}
+       })}
+     </div>
     </div>
   )
 }
